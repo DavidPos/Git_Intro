e1c9bab4ebb6a7eb1e1290b95c44aec20e9bd1df
David Postlethwaite <bigdavepos@gmail.com>
2020-11-30 20:23:54 -0800

    Update all_checks.py
    Added up to push remote lesson, includes wrapper function, checks done via a list.

--- all_checks.py
+++ all_checks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 import os
 import shutil
@@ -14,15 +14,24 @@ def check_disk_full(disk, min_gb, min_percent):
     percent_free = 100 * du.free / du.total
     #Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free< min_percent or gigabytes_free < min_gb:
+    if gigabytes_free < min_gb or percent_free < min_percent:
         return True
     return False
 
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def main():
-    if check_reboot():
-        print("Pending Reboot")
-        sys.exit(1)
-    if check_disk_full(disk = "/", min_gb=2,min_percent=10):
-        print("Disk full.")
-        sys.exit(1)
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root Partition is full.")
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)    
+    print("Everything is OK")
+    sys.exit(0)
+
+main()
